44c3fc44c3c3435b50b5ad7d3b201a4b465ee39d
SydneyKelley <kelley.sydney23@gmail.com>
2023-09-19 14:41:09 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Sydney Kelley>
+ *        Date: <09-19-2023>
+ * Description: Code for lab 2 that will print the statement "Hello Git"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
